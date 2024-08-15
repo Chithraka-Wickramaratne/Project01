677d8116976952f5712ccef3e79ae3d2d3f6e541
Chithraka-Wickramaratne <wickramaratnechithraka@gmail.com>
2024-08-15 14:07:56 +0530

    Create home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import ReportPage from './pages/ReportPage';
+//import ReportPage from './pages/ReportPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <dev>
-      <ReportPage/>
+      <HomePage/>
     </dev>
   );
 }
